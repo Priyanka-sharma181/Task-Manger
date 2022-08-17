c70eafbf56a1712815a3b3f9270b8011a620f328
Priyanka-sharma181 <Priyanka-sharma181>
2022-08-17 18:23:07 +0530

    bcrpyt password

--- src/model/user.js
+++ src/model/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const validator =  require("validator")
-
+const bcrypt = require("bcrypt")
 const UserSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -39,5 +39,12 @@ const UserSchema = new mongoose.Schema({
     }
 })
  
+UserSchema.pre('save', async function(next){
+    user = this
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
 const User = mongoose.model("User",UserSchema)
 module.exports = User
